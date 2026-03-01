276473df7d7993b8a5dc6fb4281f8465d429fa0b
jolantadjatlova <jolanta.djatlova@icloud.com>
2026-03-01 14:33:22 +0000

    add home app and base template

--- glacier_gear/urls.py
+++ glacier_gear/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.account.urls')),
+    path('', include('home.urls')),
 ]
